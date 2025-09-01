fab73efe5887aa1c4c4ea5bd61a160526a7be39c
placeyeah <placeyeah@gmail.com>
2025-09-01 17:12:12 +0900

    복습충분히한 후 커밋

--- studyPro/src/days01/Ex02.java
+++ studyPro/src/days01/Ex02.java
@@ -4,7 +4,10 @@ public class Ex02 {
 
 	public static void main(String[] args) {
 		System.out.println("복습");
-
+		System.out.println("복습");
+		System.out.println("복습");
+		System.out.println("복습");
+		System.out.println("복습");
 	}
 
 }
