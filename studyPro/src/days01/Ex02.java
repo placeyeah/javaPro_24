f4fdc63210c4af5ac026da4e7c0976ef831a0586
placeyeah <placeyeah@gmail.com>
2025-09-01 17:22:50 +0900

    Ex01.java 복습

---
+++ studyPro/src/days01/Ex02.java
@@ -0,0 +1,15 @@
+package days01;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		System.out.println("교육원 복습");
+		System.out.println("교육원 복습");
+		System.out.println("교육원 복습");
+		System.out.println("교육원 복습");
+		System.out.println("교육원 복습");
+		System.out.println("교육원 복습");
+
+	}
+
+}
